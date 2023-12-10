608b58c7b309309165c959da51228c2d4a74f8ec
ertiti11 <vatitiprieto11@gmail.com>
2023-12-10 03:20:24 +0100

    Refactor ClientWorks component to use a table for displaying works

--- src/pages/ClientWorks.jsx
+++ src/pages/ClientWorks.jsx
@@ -23,26 +23,108 @@ export default function ClientWorks() {
   }, [fetchData]);
 
   return (
-    <div className="w-full flex ">
-      {error && <div className="text-red-500">{error}</div>}
-      {console.log(works)}
-      <div className=" m-20 w-full">
-        <div className="flex flex-col items-center">
-          <h1 className="text-4xl font-bold">Trabajos</h1>
-          <div className="w-full flex flex-col items-center">
-            {works?.items.map((work) => (
-              <div
-                key={work.id}
-                className="w-full flex flex-col items-center bg-gray-500 p-4 m-4 rounded-md"
+    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+      <thead className="bg-gray-50 dark:bg-gray-800">
+        <tr>
+          <th
+            scope="col"
+            className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          >
+            <button className="flex items-center gap-x-3 focus:outline-none">
+              <span>Client</span>
+
+              <svg
+                className="h-3"
+                viewBox="0 0 10 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <h1 className="text-2xl font-bold">{work.title}</h1>
-                <p className="text-xl">{work.description}</p>
-                <p className="text-xl">{work.price}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+                <path
+                  d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="0.1"
+                />
+                <path
+                  d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="0.1"
+                />
+                <path
+                  d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="0.3"
+                />
+              </svg>
+            </button>
+          </th>
+
+          <th
+            scope="col"
+            className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          >
+            Status
+          </th>
+
+          <th
+            scope="col"
+            className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          >
+            Email
+          </th>
+          <th
+            scope="col"
+            className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          >
+            price
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+        {works?.items.map((work) => {
+          return (
+            <tr key={work.id}>
+              <td className="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                <div className="flex items-center">
+                  <div className="inline-block w-10 h-10 mr-3 rounded-full text-white">
+                    <p>{work.title}</p>
+                  </div>
+                </div>
+              </td>
+              <td className="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                <div className="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                  <a>{work.status}</a>
+                </div>
+              </td>
+              <td className="px-4 py-4 text-sm whitespace-nowrap">
+                <div>
+                  <h4 className="text-gray-700 dark:text-gray-200">
+                    {work.workDate}
+                  </h4>
+                </div>
+              </td>
+              <td className="px-4 py-4 text-sm whitespace-nowrap">
+                <div className="stat">
+                  <div className="stat-value">{work.price}€</div>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
+
+// {works?.items.map((work) => (
+//   <div
+//     key={work.id}
+//     className="w-full flex flex-col items-center bg-gray-500 p-4 m-4 rounded-md"
+//   >
+//     <h1 className="text-2xl font-bold">{work.title}</h1>
+//     <p className="text-xl">{work.description}</p>
+//     <p className="text-xl">{work.price}</p>
+//   </div>
+// ))}
