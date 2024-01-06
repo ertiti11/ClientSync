5ad28420d6bb82a3d3001eae5fab604c96102a74
ertiti11 <vatitiprieto11@gmail.com>
2024-01-06 22:52:34 +0100

    migrated completly add user form

--- src/components/AddClient.tsx
+++ src/components/AddClient.tsx
@@ -11,9 +11,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import * as React from "react"
+import AddUserForm from "./AddUserForm";
+
 
 
 export default function AddClient() {
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -26,65 +29,7 @@ export default function AddClient() {
             Crea usuarios para poder registrarlos, cuando termines de crearlo pulsa save.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              placeholder="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="lastNames" className="text-right">
-              Last names
-            </Label>
-            <Input
-              id="lastNames"
-              placeholder="García pérez"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="johndoe@gmail.com"
-              className="col-span-3"
-            
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="phone" className="text-right">
-              phone
-            </Label>
-            <Input
-              id="phone"
-              type="number"
-              placeholder="phone"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="color" className="text-right">
-              color
-            </Label>
-            <Input
-              id="color"
-              type="color"
-              className="col-span-3"
-              defaultValue={"#0003140"}
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        <AddUserForm />
       </DialogContent>
     </Dialog>
   );
---
+++ src/components/AddUserForm.tsx
@@ -0,0 +1,130 @@
+import * as React from "react";
+import { useEffect } from "react";
+import PocketBase from "pocketbase";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { DialogClose } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "name must be at least 2 characters.",
+  }),
+  email: z.string().email({ message: "Invalid email address." }),
+  phone: z.string().min(9, { message: "Invalid phone number." }),
+  lastNames: z.string().min(2, {
+    message: "Last names must be at least 2 characters.",
+  }),
+  color: z.string().min(2, {
+    message: "Color must be at least 2 characters.",
+  }),
+  date: z.date(),
+});
+
+export default function AddUserForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      lastNames: "",
+      color: "",
+      date: new Date(),
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>name</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input placeholder="Phone" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastNames"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last names</FormLabel>
+              <FormControl>
+                <Input placeholder="doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="color"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Color</FormLabel>
+              <FormControl>
+                <Input type="color" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+
+  // 2. Define a submit handler.
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(data);
+    const pb = new PocketBase("https://clients.pockethost.io");
+
+    await pb.collection("clients").create(data);
+  }
+}
