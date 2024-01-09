639d0aedb6189303b56002978e957284060c3c4a
ertiti11 <vatitiprieto11@gmail.com>
2024-01-09 17:22:46 +0100

    Remove daisyui package and update clients.js

--- src/api/clients.js
+++ src/api/clients.js
@@ -2,7 +2,9 @@ import PocketBase from "pocketbase";
 
 export const pb = new PocketBase("https://clients.pockethost.io");
 
-export const clients = await pb.collection("clients").getList(1, 50);
+export const clients = async () => {
+  return await pb.collection("clients").getList(1, 50);
+};
 
 // export function getClientData(id) {
 //   return pb.collection("clients").getOne(id);
